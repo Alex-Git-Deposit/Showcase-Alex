484a70a99dbe44d29fe704ea20eb952a989b8ea1
Alex-Git-Deposit <alexfleig777@gmail.com>
2024-07-02 11:25:06 +0200

    Add files via upload

---
+++ P_Forms/P_Forms/Form1.cs
@@ -0,0 +1,15 @@
+namespace P_Forms
+{
+    public partial class SC : Form
+    {
+        public SC()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
