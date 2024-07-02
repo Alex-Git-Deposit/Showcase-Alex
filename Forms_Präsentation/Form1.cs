c9a2cd2ad29ca00dbca128488d5e0ad18e1df95b
Alex-Git-Deposit <alexfleig777@gmail.com>
2024-07-02 11:24:40 +0200

    Add files via upload

---
+++ Forms_Präsentation/Form1.cs
@@ -0,0 +1,15 @@
+namespace Forms_Präsentation
+{
+    public partial class F_P : Form
+    {
+        public F_P()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
